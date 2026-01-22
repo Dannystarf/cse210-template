348c2b8b4e0a6fb5f0210ab38923281045c5eaa5
dannystarf <danielajayi1012042004@gmail.com>
2026-01-22 02:19:21 -1200

    edit made to resume

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -7,13 +7,13 @@ public class Resume
     {
 
     }
-    
+
     public void Display()
     {
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
 
-        foreach(Job job in _jobs)
+        foreach (Job job in _jobs)
         {
             job.Display();
         }
